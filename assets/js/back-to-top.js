6a3b6f72fe1b71c40c40f0d59dfea38793f3c3b3
kurtmarais <99024461+kurtmarais@users.noreply.github.com>
2026-08-13 12:39:41 +0200

    Update back-to-top.js

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -1,6 +1,11 @@
 const backToTop = document.querySelector(".back-to-top");
 
 if (backToTop) {
+
+  function showBackToTop() {
+    backToTop.classList.add("show");
+  }
+
   function updateBackToTop() {
     if (window.scrollY > 0) {
       backToTop.classList.add("show");
@@ -9,10 +14,22 @@ if (backToTop) {
     }
   }
 
+  /* Normal scrolling */
   window.addEventListener("scroll", updateBackToTop, { passive: true });
 
+  /* Mobile touch scrolling */
+  window.addEventListener("touchmove", function () {
+    requestAnimationFrame(function () {
+      if (window.scrollY > 0) {
+        showBackToTop();
+      }
+    });
+  }, { passive: true });
+
+  /* Set the correct initial state */
   updateBackToTop();
 
+  /* Return to top */
   backToTop.addEventListener("click", function (event) {
     event.preventDefault();
 
